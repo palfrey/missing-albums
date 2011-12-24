bdeed369d1b96fbe52ac876ca144a6bc420a7932
Tom Parker <palfrey@tevp.net>
2011-12-24 23:40:32 +0000

    amazon: Handle results with multiple items and non-CD bindings

--- amazon.py
+++ amazon.py
@@ -5,27 +5,32 @@ from lxml import etree
 keys = [x.strip() for x in file(".amazonkey").readlines()]
 amazon = amazonproduct.API(keys[0], keys[1], "uk")
 
-def searchByTitle(artist, album):
+def _empty(album):
 	ret = {}
+	ret["title"] = album
+	ret["url"] = None
+	ret["image"] = None
+	ret["amazon_new"] = None
+	return ret
+
+def searchByTitle(artist, album):
 	try:
 		root = amazon.item_search("Music", Artist=artist, Title=album, ResponseGroup="Small,ItemAttributes,Images,Offers", AssociateTag = keys[2], MerchantId = "Amazon", Condition = "New")
 	except amazonproduct.errors.NoExactMatchesFound:
-		ret["title"] = album
-		ret["url"] = None
-		ret["image"] = None
-		ret["amazon_new"] = None
-		return ret
+		return _empty(album)
 
 	page = root.page(1)
+	ret = {}
 
-	binding = page.Items.Item.ItemAttributes.Binding
-	if binding!="Audio CD":
-		file("dump","wb").write(etree.tostring(page, pretty_print=True))
-		raise Exception, binding
+	for item in page.Items.Item:
+		binding = item.ItemAttributes.Binding
+		if binding!="Audio CD":
+			continue
 
-	ret["title"] = page.Items.Item.ItemAttributes.Title
-	ret["url"] = page.Items.Item.DetailPageURL
-	ret["image"] = page.Items.Item.LargeImage.URL
-	#ret["lowest_new"] = int(page.Items.Item.OfferSummary.LowestNewPrice.Amount)
-	ret["amazon_new"] = int(page.Items.Item.Offers.Offer.OfferListing.Price.Amount)
-	return ret
+		ret["title"] = item.ItemAttributes.Title
+		ret["url"] = item.DetailPageURL
+		ret["image"] = item.LargeImage.URL
+		#ret["lowest_new"] = int(item.OfferSummary.LowestNewPrice.Amount)
+		ret["amazon_new"] = int(item.Offers.Offer.OfferListing.Price.Amount)
+		return ret
+	return _empty(album)
